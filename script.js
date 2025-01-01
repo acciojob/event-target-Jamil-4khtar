d761497d6e998453e6fd4fb24a050624a604b98e
Jamil-4khtar <156568813+Jamil-4khtar@users.noreply.github.com>
2025-01-01 14:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,47 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+		if(!this.listeners.has(event)) {
+		  this.listeners.set(event, [])
+		}
+		if (!this.listeners.get(event).includes(callback)) {
+			this.listeners.get(event).push(callback);
+		}
     }
   
     removeEventListener(event, callback) {
-      
+		if (this.listeners.has(event)) {
+			let listeners = this.listeners.get(event);
+			let index = listeners.indexOf(callback);
+			if (index != -1) {
+				listeners.splice(index, 1);
+			}
+		}
     }
   
     dispatchEvent(event) {
-    
+	    if(this.listeners.has(event)) {
+			let listeners = this.listeners.get(event);
+			for (let i = 0; i < listeners.length; i++) {
+				listener();
+			}
+		}
     }
     
   }
+
+const target = new EventTarget();
+const logHello = () => console.log('hello');
+const logWorld = () => console.log('world');
+
+target.addEventListener('hello', logHello);
+target.addEventListener('world', logWorld);
+
+target.dispatchEvent('hello'); // logs 'hello'
+target.dispatchEvent('world'); // logs 'world'
+
+target.removeEventListener('hello', logHello);
+
+target.dispatchEvent('hello'); // does nothing
+target.dispatchEvent('world'); // logs 'world'
   
